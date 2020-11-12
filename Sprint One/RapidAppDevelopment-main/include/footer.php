86a76d9d1c686a81744097e28cd1cf8e9f2d5c8f
Domain Student <student@cite.wa.edu.au>
2020-11-12 09:47:22 +0800

    New Layout

--- Sprint One/RapidAppDevelopment-main/include/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-// echoing the footer
-echo '<footer>
-<div class="fmiddle">
-    <h4>SMT Movie Rentals</h4>
-    Thank you for using SMT Movie Rentals
-</div>
-</footer>';
-?>
--- Sprint One/RapidAppDevelopment-main/include/nav.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// echoing the nav bar
-echo '<nav>
-<!-- <li><img height="50px" src="websitelogo.png" ></li> -->
-<li><a href="index.php">HOME</a></li>
-<li><a href="search_movie.php">SEARCH</a></li>
-<li><a href="add_movie.php">ADD</a></li>
-</nav>';
-?>
